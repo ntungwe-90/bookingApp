560a7097d829d587556227fe5b054f75082e1ce6
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2022-02-15 10:26:06 +0000

    added styling

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 // const csrf = require('csurf')
 const expressLayouts = require("express-ejs-layouts");
 
-// const session = require('express-session')
-// const cookieParser = require('cookie-parser')
+ const session = require('express-session')
+//  const cookieParser = require('cookie-parser')
 
 const app = express();
 
@@ -21,13 +21,21 @@ app.set("view engine", "ejs");
 
 // const csrfProtection = csrf({cookie:true})
 // app.use(csrfProtection)
-// app.use(session({
-//     secret:process.env.SECRETE,
-//     reserve:false,
-//     saveUninitialized:true,
-//     cookie:{secure:true}
-// })
-// )
+app.use(session({
+    secret: process.env.SECRET,
+    reserve: true,
+    saveUninitialized: true,
+    cookie:{secure: false}
+})
+)
+
+// cookie parser
+// app.use(cookieParser())
+// cookie-session
+// app.use(cookieSession({
+//     name : 'session',
+//     keys : process.env.KEYS,
+// }))
 
 const route = require("./server/routes/pagesRoute");
 app.use("/", route);
@@ -40,6 +48,7 @@ app.use("/slots", slotRoute);
 
 
 const loginRoute = require("./server/routes/loginRoutes");
+const { cookie } = require("express/lib/response");
 app.use("/", loginRoute)
 
 // const failedBooking = require("./server/routes/failedBookingRoutes");
